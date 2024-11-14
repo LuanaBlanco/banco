502a9ce30d36106fa805700c69bcb69e018a255b
Luana Blanco <blancoluana@gmail.com>
2024-11-14 13:11:28 -0300

    agreagndo banco

---
+++ src/main/java/com/pip/banco/models/cuentaEntity.java
@@ -0,0 +1,46 @@
+package com.pip.banco.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cuenta")
+public class cuentaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String Cuenta;
+    private int numeroDeCuenta;
+    private String tipoDeCuenta;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCuenta() {
+        return Cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        Cuenta = cuenta;
+    }
+
+    public int getNumeroDeCuenta() {
+        return numeroDeCuenta;
+    }
+
+    public void setNumeroDeCuenta(int numeroDeCuenta) {
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+
+    public String getTipoDeCuenta() {
+        return tipoDeCuenta;
+    }
+
+    public void setTipoDeCuenta(String tipoDeCuenta) {
+        this.tipoDeCuenta = tipoDeCuenta;
+    }
+}
---
+++ src/test/java/com/pip/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pip.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
